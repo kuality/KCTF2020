e97a49fb9947add17a34377366e5d63a76a1dbd9
SParK <36344028+dtqdtq01@users.noreply.github.com>
2020-11-15 02:16:55 +0900

    Add files via upload

---
+++ misc/FUNFUN/share/escape.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python3
+import random
+
+def main():
+    op_box = ["+","-","*","//"] 
+    print("Welcome To Four rules calculation Game in Python")
+    count = 0
+
+    while(True):
+        if(count == 100):
+            print("What is Your Name : ")
+            name = input()
+            for keyword in ['eval', 'exec', 'import', 'open', 'os', 'read', 'system', 'write']:
+                if keyword in name:
+                    print("No!!!!")
+                    exit(0)
+                else:
+                    exec(name)
+
+        #x = random.randint(#####)
+        #y = random.randint(#####)
+        #z = random.randint(#####)
+        Random_Question = str(x)+str(op_box[z])+str(y)
+        print(Random_Question, '=', end='')
+        answer = int(input())
+        if answer == eval(Random_Question):
+            print("Right!!!")
+            count += 1
+        else:
+            print("Try again!!")
+        
+if __name__ == "__main__":
+    main()
